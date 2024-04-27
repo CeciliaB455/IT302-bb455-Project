38f2d04acf81801cc5c449ba93034b6ec997916f
Cecilia Bruce <156833930+CeciliaB455@users.noreply.github.com>
2024-04-26 22:04:32 -0400

    Delete api directory

--- api/comics.route.js
+++
@@ -1,9 +0,0 @@
-//Cecilia Bruce, 2/29/24, IT302 Section 002, Phase 2 Assignment, bb455@njit.edu
-import express from 'express'
-import ComicsController from './comics.controller.js'
-
-const router = express.Router()
-
-router.route('/').get(ComicsController.apiGetComics)
-
-export default router
